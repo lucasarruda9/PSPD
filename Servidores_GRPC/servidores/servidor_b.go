227c379992d1e2f8d06be105f2df6ace016791ac
johnaopedro <johnaopedro@gmail.com>
2026-06-08 12:50:40 -0300

    fix: guardar arquivos no dataset

--- Servidores_GRPC/servidores/servidor_b.go
+++ Servidores_GRPC/servidores/servidor_b.go
@@ -8,6 +8,7 @@ import (
     "io/ioutil"
     "log"
     "net"
+    "strings"
 
     "github.com/suyashkumar/dicom"
     "github.com/suyashkumar/dicom/pkg/tag"
@@ -165,7 +166,8 @@ func (s *serverB) UploadExam(stream pb.Pipeline_UploadExamServer) error {
         if err != nil {
             return err
         }
-        nomeDestinoArquivo := fmt.Sprintf("%s/upload_fatia_%s.dcm", DiretorioArquivos, fatiaRecebida.SliceId)
+        baseName := strings.TrimSuffix(fatiaRecebida.SliceId, ".dcm")
+        nomeDestinoArquivo := fmt.Sprintf("%s/upload_fatia_%s.dcm", DiretorioArquivos, baseName)
         _ = ioutil.WriteFile(nomeDestinoArquivo, fatiaRecebida.Data, 0644)
 
         quantidadeFatiasRecebidas++
